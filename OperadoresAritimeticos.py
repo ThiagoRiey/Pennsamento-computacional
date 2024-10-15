f77b12db5f709f586c33637817429e7ab4d2f922
ThiagoRiey <153005132+ThiagoRiey@users.noreply.github.com>
2024-10-15 15:25:06 -0400

    Create OperadoresAritimeticos.py

---
+++ OperadoresAritimeticos.py
@@ -0,0 +1,16 @@
+a = 60
+b = 25
+
+soma = a + b
+subitração = a - b
+multiplicação = a * b
+divisão = a / b
+resto_da_divisão = a % b
+potencia = a ** b
+
+print('soma: ', + soma)
+print('subitração: ', + subitração)
+print('multiplicação: ', + multiplicação)
+print('divisão: ', + divisão)
+print('resto_da_divisão: ', + resto_da_divisão)
+print('potencia: ', + potencia)
