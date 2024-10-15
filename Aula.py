7b602019345de5713e7cf6f81a04de2a532d3488
ThiagoRiey <153005132+ThiagoRiey@users.noreply.github.com>
2024-10-15 14:35:52 -0400

    Aula.py

---
+++ Aula.py
@@ -0,0 +1,22 @@
+#int
+idade = 24
+temperatura = 18
+
+#float
+peso = 85.3
+altura = 1.85
+
+#String
+nome = 'Thiago'
+sobrenome = 'Riey
+
+#bool
+verdadeiro = True
+falso = False
+print(verdadeiro and verdadiro)
+print(verdadeiro and falso)
+
+#NoneType
+x = None
+
+type(temperatura)
