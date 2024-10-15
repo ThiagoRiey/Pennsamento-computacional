66cdadd18a56b96606b49923ff6594461f379508
ThiagoRiey <153005132+ThiagoRiey@users.noreply.github.com>
2024-10-15 15:04:12 -0400

    Update EstruturaDeRepetição.py

--- EstruturaDeRepetição.py
+++ EstruturaDeRepetição.py
@@ -1,2 +1,14 @@
 for i in range(5)
-  print('Olá!!)
+  print('Olá!!')
+
+____________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+soma = 0
+
+provas = int(input('Digite o número de provas:'))
+for i in range(provas):
+  nota = int(input(f'Digite a sua nota da p{i+1}: '))
+  soma = soma + nota
+
+media = soma/provas
+print(media
