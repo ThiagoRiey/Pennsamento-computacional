fb35d7eb3a398297a1d58698a3a0a6d4b05ac207
ThiagoRiey <153005132+ThiagoRiey@users.noreply.github.com>
2024-10-15 15:14:27 -0400

    Create Random2.py
    Gerando números pseudo aleatórios:
    Python random O módulo Python random é utilizado quando queremos gerar números pseudo-aleatórios, semelhantes a números aleatórios, entretanto, são produzidos por meio de algoritmos que utilizam um valor de base para gerar outros valores aleatórios.
    
    Esse é um recurso muito utilizado em algoritmos de cálculos estatísticos e de teorias de probabilidade, por exemplo. Além disso, o módulo random contém uma série de métodos auxiliares. Para utilizar esse recurso, devemos importar o módulo no código correspondente. É importante dizer que ao utilizar a mesma base e depurarmos o programa, o resultado gerado será sempre o mesmo. Veja um pequeno exemplo:
    
    https://blog.betrybe.com/python/

---
+++ Random2.py
@@ -0,0 +1,9 @@
+import random # necessário para utilizar o módulo ramdom
+
+random.seed(20)
+print(random.random()) # Resultados: 0.2
+print(random.random()) # Resultado: 3
+
+random.seed(20)
+print(random.random()) # resultado: 0.9056396761745207
+print(random.random()) # resultado: 0.6862541570267026
